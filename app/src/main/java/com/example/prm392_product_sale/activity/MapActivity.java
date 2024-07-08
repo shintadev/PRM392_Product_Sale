ef90501c962510849ae677683ea889daa817f902
shintadev <84170345+shintadev@users.noreply.github.com>
2024-07-08 23:24:10 +0700

    update: map activity

--- app/src/main/java/com/example/prm392_product_sale/activity/MapActivity.java
+++ app/src/main/java/com/example/prm392_product_sale/activity/MapActivity.java
@@ -6,17 +6,29 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentActivity;
 
 import com.example.prm392_product_sale.R;
 import com.example.prm392_product_sale.databinding.ActivityChatBinding;
 import com.example.prm392_product_sale.databinding.ActivityMapBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private final LatLng STORE_LOCATION = new LatLng(10.841416800000001, 106.81007447258705); // FPTU HCMC
+
     ActivityMapBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivityMapBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -26,7 +38,21 @@ public class MapActivity extends AppCompatActivity {
         // Enable the Up button
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Location");
         }
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker at the store location and move the camera
+        mMap.addMarker(new MarkerOptions().position(STORE_LOCATION).title("Store Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(STORE_LOCATION, 15));
     }
 
     @Override
